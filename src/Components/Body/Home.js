46822803ff632d38152d29d3104078ffa0a08768
ankit <ankitiwari26@gmail.com>
2024-05-28 09:33:23 +0530

    Last Commit for expense tracker

--- src/Components/Body/Home.js
+++ src/Components/Body/Home.js
@@ -1,43 +1,179 @@
-// // import React, { useCallback, useState, useEffect } from "react";
+// // // import React, { useCallback, useState, useEffect } from "react";
 
+// // // import Protected from "../Protected";
+// // // import ExpenseForm from "./ExpenseForm";
+// // // import ExpenseList from "./ExpenceList";
+
+// // // const Home = () => {
+
+// // //   const [expenses, setExpenses] = useState([]);
+// // //   const [loading, setLoading] = useState(false);
+
+// // //   const fetchExpenseData = useCallback(() => {
+// // //     setLoading(true);
+// // //     fetch(
+// // //       "https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata.json"
+// // //     )
+// // //       .then((response) => response.json())
+// // //       .then((data) => {
+// // //         if (data) {
+// // //           const transformedExpense = Object.keys(data).map((key) => {
+// // //             const expense = data[key];
+// // //             return {
+// // //               id: key,
+// // //               amount: expense.amount,
+// // //               description: expense.description,
+// // //               category: expense.category,
+// // //             };
+// // //           });
+// // //           setExpenses(transformedExpense);
+// // //         }
+// // //         setLoading(false);
+// // //       })
+// // //       .catch((error) => {
+// // //         console.log("Something went wrong ....Retrying ", error);
+// // //         setLoading(false);
+// // //       });
+// // //   }, []);
+
+// // //   const handleAddExpense = async (expense) => {
+// // //     setLoading(true);
+// // //     try {
+// // //       const response = await fetch(
+// // //         "https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata.json",
+// // //         {
+// // //           method: "POST",
+// // //           headers: {
+// // //             "Content-Type": "application/json",
+// // //           },
+// // //           body: JSON.stringify(expense),
+// // //         }
+// // //       );
+// // //       if (response.ok) {
+// // //         fetchExpenseData();
+// // //       }
+// // //     } catch (error) {
+// // //       console.log("Error adding expense: ", error);
+// // //       setLoading(false);
+// // //     }
+// // //   };
+
+// // //   const handleEditExpense = async (expense) => {
+// // //     setLoading(true);
+// // //     try {
+// // //       const response = await fetch(
+// // //         `https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata/${expense.id}.json`,
+// // //         {
+// // //           method: "PUT",
+// // //           headers: {
+// // //             "Content-Type": "application/json",
+// // //           },
+// // //           body: JSON.stringify({
+// // //             amount: expense.amount,
+// // //             description: expense.description,
+// // //             category: expense.category,
+// // //           }),
+// // //         }
+// // //       );
+// // //       if (response.ok) {
+// // //         fetchExpenseData();
+// // //       }
+// // //       console.log("Expense edited from the list.");
+// // //     } catch (error) {
+// // //       console.log("Error editing expense: ", error);
+// // //       setLoading(false);
+// // //     }
+// // //   };
+
+// // //   const handleDeleteExpense = async (expenseId) => {
+// // //     setLoading(true);
+// // //     try {
+// // //       const response = await fetch(
+// // //         `https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata/${expenseId}.json`,
+// // //         {
+// // //           method: "DELETE",
+// // //         }
+// // //       );
+// // //       if (response.ok) {
+// // //         fetchExpenseData();
+// // //       }
+// // //       console.log("Expense deleted from the list.");
+// // //     } catch (error) {
+// // //       console.log("Error deleting expense: ", error);
+// // //       setLoading(false);
+// // //     }
+// // //   };
+
+// // //   useEffect(() => {
+// // //     fetchExpenseData();
+// // //   }, [fetchExpenseData]);
+
+// // //   return (
+// // //     <div>
+// // //       <h2>Welcome to Expense Tracker</h2>
+// // //       <Protected>
+// // //         <ExpenseForm addExpense={handleAddExpense} />
+// // //         {loading ? (
+// // //           <p>Loading...</p>
+// // //         ) : (
+// // //           <ExpenseList
+// // //             expenses={expenses}
+// // //             editExpense={handleEditExpense}
+// // //             deleteExpense={handleDeleteExpense}
+// // //           />
+// // //         )}
+// // //       </Protected>
+// // //     </div>
+// // //   );
+// // // };
+
+// // // export default Home;
+
+// // import React, { useCallback, useEffect } from "react";
+// // import { useDispatch, useSelector } from "react-redux";
+// // import { Button } from "react-bootstrap";
 // // import Protected from "../Protected";
 // // import ExpenseForm from "./ExpenseForm";
-// // import ExpenseList from "./ExpenceList";
+// // import ExpenseList from "./ExpenseList";
+// // import {
+// //   setExpenses,
+// //   addExpense,
+// //   editExpense,
+// //   deleteExpense,
+// //   toggleTheme,
+// // } from "../../store/index";
 
 // // const Home = () => {
-
-// //   const [expenses, setExpenses] = useState([]);
-// //   const [loading, setLoading] = useState(false);
+// //   const dispatch = useDispatch();
+// //   const expenses = useSelector((state) => state.expenses.list);
+// //   const totalAmount = useSelector((state) => state.expenses.totalAmount);
+// //   const loading = useSelector((state) => state.expenses.loading);
+// //   const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
 
 // //   const fetchExpenseData = useCallback(() => {
-// //     setLoading(true);
+// //     dispatch({ type: "expenses/loading", payload: true });
 // //     fetch(
 // //       "https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata.json"
 // //     )
 // //       .then((response) => response.json())
 // //       .then((data) => {
 // //         if (data) {
-// //           const transformedExpense = Object.keys(data).map((key) => {
-// //             const expense = data[key];
-// //             return {
-// //               id: key,
-// //               amount: expense.amount,
-// //               description: expense.description,
-// //               category: expense.category,
-// //             };
-// //           });
-// //           setExpenses(transformedExpense);
+// //           const transformedExpenses = Object.keys(data).map((key) => ({
+// //             id: key,
+// //             ...data[key],
+// //           }));
+// //           dispatch(setExpenses(transformedExpenses));
 // //         }
-// //         setLoading(false);
+// //         dispatch({ type: "expenses/loading", payload: false });
 // //       })
 // //       .catch((error) => {
 // //         console.log("Something went wrong ....Retrying ", error);
-// //         setLoading(false);
+// //         dispatch({ type: "expenses/loading", payload: false });
 // //       });
-// //   }, []);
+// //   }, [dispatch]);
 
 // //   const handleAddExpense = async (expense) => {
-// //     setLoading(true);
+// //     dispatch({ type: "expenses/loading", payload: true });
 // //     try {
 // //       const response = await fetch(
 // //         "https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata.json",
@@ -52,56 +188,32 @@
 // //       if (response.ok) {
 // //         fetchExpenseData();
 // //       }
+// //       dispatch(addExpense());
 // //     } catch (error) {
 // //       console.log("Error adding expense: ", error);
-// //       setLoading(false);
+// //       dispatch({ type: "expenses/loading", payload: false });
 // //     }
 // //   };
 
-// //   const handleEditExpense = async (expense) => {
-// //     setLoading(true);
-// //     try {
-// //       const response = await fetch(
-// //         `https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata/${expense.id}.json`,
-// //         {
-// //           method: "PUT",
-// //           headers: {
-// //             "Content-Type": "application/json",
-// //           },
-// //           body: JSON.stringify({
-// //             amount: expense.amount,
-// //             description: expense.description,
-// //             category: expense.category,
-// //           }),
-// //         }
-// //       );
-// //       if (response.ok) {
-// //         fetchExpenseData();
-// //       }
-// //       console.log("Expense edited from the list.");
-// //     } catch (error) {
-// //       console.log("Error editing expense: ", error);
-// //       setLoading(false);
-// //     }
+// //   const handleActivatePremium = () => {
+// //     dispatch(toggleTheme());
+// //     console.log("Activate primium button clicked");
 // //   };
 
-// //   const handleDeleteExpense = async (expenseId) => {
-// //     setLoading(true);
-// //     try {
-// //       const response = await fetch(
-// //         `https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata/${expenseId}.json`,
-// //         {
-// //           method: "DELETE",
-// //         }
-// //       );
-// //       if (response.ok) {
-// //         fetchExpenseData();
-// //       }
-// //       console.log("Expense deleted from the list.");
-// //     } catch (error) {
-// //       console.log("Error deleting expense: ", error);
-// //       setLoading(false);
-// //     }
+// //   const handleDownloadCSV = () => {
+// //     const csvContent =
+// //       "data:text/csv;charset=utf-8," +
+// //       "Amount,Description,Category\n" +
+// //       expenses
+// //         .map((exp) => `${exp.amount},${exp.description},${exp.category}`)
+// //         .join("\n");
+// //     const encodedUri = encodeURI(csvContent);
+// //     const link = document.createElement("a");
+// //     link.setAttribute("href", encodedUri);
+// //     link.setAttribute("download", "expenses.csv");
+// //     document.body.appendChild(link);
+// //     link.click();
+// //     document.body.removeChild(link);
 // //   };
 
 // //   useEffect(() => {
@@ -109,19 +221,21 @@
 // //   }, [fetchExpenseData]);
 
 // //   return (
-// //     <div>
+// //     <div className={isDarkTheme ? "dark-theme" : "light-theme"}>
 // //       <h2>Welcome to Expense Tracker</h2>
 // //       <Protected>
 // //         <ExpenseForm addExpense={handleAddExpense} />
-// //         {loading ? (
-// //           <p>Loading...</p>
-// //         ) : (
-// //           <ExpenseList
-// //             expenses={expenses}
-// //             editExpense={handleEditExpense}
-// //             deleteExpense={handleDeleteExpense}
-// //           />
+// //         {totalAmount > 10000 && (
+// //           <div>
+// //             <Button variant="warning" onClick={handleActivatePremium}>
+// //               Activate Premium
+// //             </Button>
+// //             <Button variant="secondary" onClick={handleDownloadCSV}>
+// //               Download File
+// //             </Button>
+// //           </div>
 // //         )}
+// //         {loading ? <p>Loading...</p> : <ExpenseList expenses={expenses} />}
 // //       </Protected>
 // //     </div>
 // //   );
@@ -129,7 +243,126 @@
 
 // // export default Home;
 
-import React, { useCallback, useEffect } from "react";
+// import React, { useCallback, useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { Button } from "react-bootstrap";
+// import Protected from "../Protected";
+// import ExpenseForm from "./ExpenseForm";
+// import ExpenseList from "./ExpenseList";
+// import {
+//   setExpenses,
+//   addExpense,
+//   editExpense,
+//   deleteExpense,
+//   toggleTheme,
+// } from "../../store/index";
+
+// const Home = () => {
+//   const dispatch = useDispatch();
+//   const expenses = useSelector((state) => state.expenses.list);
+//   const totalAmount = useSelector((state) => state.expenses.totalAmount);
+//   const loading = useSelector((state) => state.expenses.loading);
+//   const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
+
+//   const fetchExpenseData = useCallback(() => {
+//     dispatch({ type: "expenses/loading", payload: true });
+//     fetch(
+//       "https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata.json"
+//     )
+//       .then((response) => response.json())
+//       .then((data) => {
+//         if (data) {
+//           const transformedExpenses = Object.keys(data).map((key) => ({
+//             id: key,
+//             ...data[key],
+//           }));
+//           dispatch(setExpenses(transformedExpenses));
+//         }
+//         dispatch({ type: "expenses/loading", payload: false });
+//       })
+//       .catch((error) => {
+//         console.log("Something went wrong ....Retrying ", error);
+//         dispatch({ type: "expenses/loading", payload: false });
+//       });
+//   }, [dispatch]);
+
+//   const handleAddExpense = async (expense) => {
+//     dispatch({ type: "expenses/loading", payload: true });
+//     try {
+//       const response = await fetch(
+//         "https://expencetracker-90de3-default-rtdb.firebaseio.com/expensedata.json",
+//         {
+//           method: "POST",
+//           headers: {
+//             "Content-Type": "application/json",
+//           },
+//           body: JSON.stringify(expense),
+//         }
+//       );
+//       if (response.ok) {
+//         fetchExpenseData();
+//       }
+//       dispatch(addExpense());
+//     } catch (error) {
+//       console.log("Error adding expense: ", error);
+//       dispatch({ type: "expenses/loading", payload: false });
+//     }
+//   };
+
+//   const handleActivatePremium = () => {
+//     dispatch(toggleTheme());
+//     console.log("Activate Premium button clicked");
+//   };
+
+//   const handleDownloadCSV = () => {
+//     const csvContent =
+//       "data:text/csv;charset=utf-8," +
+//       "Amount,Description,Category\n" +
+//       expenses
+//         .map((exp) => `${exp.amount},${exp.description},${exp.category}`)
+//         .join("\n");
+//     const encodedUri = encodeURI(csvContent);
+//     const link = document.createElement("a");
+//     link.setAttribute("href", encodedUri);
+//     link.setAttribute("download", "expenses.csv");
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+//   };
+
+//   useEffect(() => {
+//     fetchExpenseData();
+//   }, [fetchExpenseData]);
+
+//   return (
+//     <div
+//       style={{
+//         backgroundColor: isDarkTheme ? "#333" : "#fff",
+//         color: isDarkTheme ? "#fff" : "#000",
+//       }}
+//     >
+//       <h2>Welcome to Expense Tracker</h2>
+//       <Protected>
+//         <ExpenseForm addExpense={handleAddExpense} />
+//         {totalAmount > 10000 && (
+//           <div>
+//             <Button variant="warning" onClick={handleActivatePremium}>
+//               Activate Premium
+//             </Button>
+//             <Button variant="secondary" onClick={handleDownloadCSV}>
+//               Download File
+//             </Button>
+//           </div>
+//         )}
+//         {loading ? <p>Loading...</p> : <ExpenseList expenses={expenses} />}
+//       </Protected>
+//     </div>
+//   );
+// };
+
+// export default Home;
+
+import React, { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Button } from "react-bootstrap";
 import Protected from "../Protected";
@@ -142,7 +375,6 @@ import {
   deleteExpense,
   toggleTheme,
 } from "../../store/index";
-import "./Home.css";
 
 const Home = () => {
   const dispatch = useDispatch();
@@ -150,6 +382,7 @@ const Home = () => {
   const totalAmount = useSelector((state) => state.expenses.totalAmount);
   const loading = useSelector((state) => state.expenses.loading);
   const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
+  const [premiumActivated, setPremiumActivated] = useState(false);
 
   const fetchExpenseData = useCallback(() => {
     dispatch({ type: "expenses/loading", payload: true });
@@ -197,8 +430,7 @@ const Home = () => {
   };
 
   const handleActivatePremium = () => {
-    dispatch(toggleTheme());
-    console.log("Activate primium button clicked");
+    setPremiumActivated(true);
   };
 
   const handleDownloadCSV = () => {
@@ -222,7 +454,12 @@ const Home = () => {
   }, [fetchExpenseData]);
 
   return (
-    <div className={isDarkTheme ? "dark-theme" : "light-theme"}>
+    <div
+      style={{
+        backgroundColor: isDarkTheme ? "#333" : "#fff",
+        color: isDarkTheme ? "#fff" : "#000",
+      }}
+    >
       <h2>Welcome to Expense Tracker</h2>
       <Protected>
         <ExpenseForm addExpense={handleAddExpense} />
@@ -230,6 +467,16 @@ const Home = () => {
           <div>
             <Button variant="warning" onClick={handleActivatePremium}>
               Activate Premium
+            </Button>
+          </div>
+        )}
+        {premiumActivated && (
+          <div>
+            <Button
+              variant="outline-secondary"
+              onClick={() => dispatch(toggleTheme())}
+            >
+              Toggle Theme
             </Button>
             <Button variant="secondary" onClick={handleDownloadCSV}>
               Download File
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,164 @@
+// // import { Button } from "react-bootstrap";
+// // import Container from "react-bootstrap/Container";
+// // import Nav from "react-bootstrap/Nav";
+// // import Navbar from "react-bootstrap/Navbar";
+// // import { Link, useNavigate } from "react-router-dom";
+// // import Logout from "../Body/Authentication/Logout";
+// // import { useState, useEffect } from "react";
+// // import UpdateProfileModal from "../Body/UpdateProfileModal";
+// // import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// // function Header() {
+// //   const [show, setShow] = useState(false);
+// //   const [isLoggedIn, setIsLoggedIn] = useState(false);
+// //   const navigate = useNavigate();
+
+// //   const handleClose = () => setShow(false);
+// //   const handleShow = () => setShow(true);
+
+// //   useEffect(() => {
+// //     const auth = getAuth();
+// //     const unsubscribe = onAuthStateChanged(auth, (user) => {
+// //       if (user) {
+// //         setIsLoggedIn(true);
+// //       } else {
+// //         setIsLoggedIn(false);
+// //       }
+// //     });
+
+// //     return () => unsubscribe();
+// //   }, []);
+
+// //   const handleHomeClick = () => {
+// //     if (isLoggedIn) {
+// //       navigate("/home");
+// //     } else {
+// //       navigate("/");
+// //     }
+// //   };
+
+// //   return (
+// //     <>
+// //       <Navbar expand="lg" className="bg-body-tertiary">
+// //         <Container>
+// //           <Navbar.Brand href="#home">Expence Tracker</Navbar.Brand>
+// //           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+// //           <Navbar.Collapse id="basic-navbar-nav">
+// //             <Nav className="me-auto">
+// //               <Nav.Link as={Link} to="#" onClick={handleHomeClick}>
+// //                 Home
+// //               </Nav.Link>
+// //               <Nav.Link as={Link} to="/">
+// //                 Login
+// //               </Nav.Link>
+// //               {/* <Button onClick={handleShow}>Update Profile</Button> */}
+
+// //               <Nav.Link onClick={handleShow}>Update Profile</Nav.Link>
+
+// //               <Logout />
+
+// //               {console.log("logout button clicked")}
+// //             </Nav>
+// //           </Navbar.Collapse>
+// //         </Container>
+// //       </Navbar>
+// //       <UpdateProfileModal show={show} handleClose={handleClose} />
+// //     </>
+// //   );
+// // }
+
+// // export default Header;
+
+// import React, { useState, useEffect } from "react";
+// import { Button } from "react-bootstrap";
+// import Container from "react-bootstrap/Container";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import { Link, useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { toggleTheme } from "../../store/index";
+// import Logout from "../Body/Authentication/Logout";
+// import UpdateProfileModal from "../Body/UpdateProfileModal";
+// import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+// function Header() {
+//   const [show, setShow] = useState(false);
+//   const [isLoggedIn, setIsLoggedIn] = useState(false);
+//   const navigate = useNavigate();
+//   const dispatch = useDispatch();
+//   const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
+
+//   const handleClose = () => setShow(false);
+//   const handleShow = () => setShow(true);
+
+//   useEffect(() => {
+//     const auth = getAuth();
+//     const unsubscribe = onAuthStateChanged(auth, (user) => {
+//       if (user) {
+//         setIsLoggedIn(true);
+//       } else {
+//         setIsLoggedIn(false);
+//       }
+//     });
+
+//     return () => unsubscribe();
+//   }, []);
+
+//   const handleHomeClick = () => {
+//     if (isLoggedIn) {
+//       navigate("/home");
+//     } else {
+//       navigate("/");
+//     }
+//   };
+
+//   const handleToggleTheme = () => {
+//     dispatch(toggleTheme());
+//   };
+
+//   return (
+//     <>
+//       <Navbar
+//         expand="lg"
+//         className={
+//           isDarkTheme ? "navbar-dark bg-dark" : "navbar-light bg-light"
+//         }
+//       >
+//         <Container>
+//           <Navbar.Brand href="#home">Expense Tracker</Navbar.Brand>
+//           <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//           <Navbar.Collapse id="basic-navbar-nav">
+//             <Nav className="me-auto">
+//               <Nav.Link as={Link} to="#" onClick={handleHomeClick}>
+//                 Home
+//               </Nav.Link>
+//               <Nav.Link as={Link} to="/">
+//                 Login
+//               </Nav.Link>
+//               <Nav.Link onClick={handleShow}>Update Profile</Nav.Link>
+//               <Logout />
+//               <Button variant="outline-secondary" onClick={handleToggleTheme}>
+//                 Toggle Theme
+//               </Button>
+//             </Nav>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//       <UpdateProfileModal show={show} handleClose={handleClose} />
+//     </>
+//   );
+// }
+
+// export default Header;
+
+import React, { useState, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 import Logout from "../Body/Authentication/Logout";
-import { useState, useEffect } from "react";
 import UpdateProfileModal from "../Body/UpdateProfileModal";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
@@ -12,6 +166,7 @@ function Header() {
   const [show, setShow] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
+  const isDarkTheme = useSelector((state) => state.theme.isDarkTheme);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -39,9 +194,14 @@ function Header() {
 
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar
+        expand="lg"
+        className={
+          isDarkTheme ? "navbar-dark bg-dark" : "navbar-light bg-light"
+        }
+      >
         <Container>
-          <Navbar.Brand href="#home">Expence Tracker</Navbar.Brand>
+          <Navbar.Brand href="#home">Expense Tracker</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
@@ -51,13 +211,8 @@ function Header() {
               <Nav.Link as={Link} to="/">
                 Login
               </Nav.Link>
-              {/* <Button onClick={handleShow}>Update Profile</Button> */}
-
               <Nav.Link onClick={handleShow}>Update Profile</Nav.Link>
-
               <Logout />
-
-              {console.log("logout button clicked")}
             </Nav>
           </Navbar.Collapse>
         </Container>
